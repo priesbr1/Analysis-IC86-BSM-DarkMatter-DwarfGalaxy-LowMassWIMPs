7d6460fc7bcfb6694deda7e1428e12507ee994bb
Brandon Michael Pries <priesbr1@dev-intel16-k80.i>
2023-07-01 17:23:14 -0400

    Efficiency improvenents, injects over background, added J_type argument, limited ns scan for background-only samples

--- scripts/signal_pdf_trials.py
+++ scripts/signal_pdf_trials.py
@@ -18,18 +18,20 @@ parser.add_argument("-p", "--sig_pdfs", type=str, default="/mnt/home/priesbr1/DM
                     dest="sig_pdfs", help="file containing signal PDFs for each mass and channel")
 parser.add_argument("-s", "--sources", type=str, default="/mnt/home/priesbr1/DM_Search/data/analysis_sources_ra_dec_jfactors.txt",
                     dest="sources", help="file containing RA, dec, and J-factor information for each source")
+parser.add_argument("-j", "--J_type", type=str, choices=["0.1","0.2","0.5","10","min","max"], default="max",
+                    dest="J_type", help="J-factor values to use based on opening half-angle")
 parser.add_argument("-c", "--channel", type=str, default="b",
                     dest="channel", help="annhiliation channel to run background trials for")
 parser.add_argument("-m", "--mass", type=int, default=10,
                     dest="mass", help="WIMP mass to rub background trials for")
+parser.add_argument("-e", "--num_bkg_events", type=int, default=1000,
+                    dest="num_bkg_events", help="number of background events to sample")
 parser.add_argument("-n", "--num_trials", type=int, default=1,
                     dest="num_trials", help="number of signal trials to run per core")
 parser.add_argument("-d", "--seed", type=int, default=None,
                     dest="seed", help="RNG seed for reproducibility")
 parser.add_argument("-o", "--outfolder", type=str, default="/mnt/home/priesbr1/DM_Search/data/trials_results/trials_sig_360deg_Jmax_distributed/",
                     dest="outfolder", help="folder to save signal trials to")
-parser.add_argument("-f", "--bkg_folder", type=str, default="/mnt/home/priesbr1/DM_Search/data/trials_results/trials_bkg_360deg_Jmax/",
-                    dest="bkg_folder", help="folder containing results from corresponding background trials")
 args = parser.parse_args()
 
 if (args.seed != None):
@@ -40,73 +42,81 @@ bkg_pdfs = bkg_pdfs.item()
 sig_pdfs = np.load(args.sig_pdfs, allow_pickle=True)
 sig_pdfs = sig_pdfs.item()
 
+def extract_source_info(sources, J_type, J_indices_map):
+    names = []
+    ra = np.array([])
+    dec = np.array([])
+    J_factors = np.array([])
+
+    J_indices = J_indices_map[J_type]
+
+    for i in range(len(sources)):
+        for idx in J_indices:
+            if not np.isnan(sources[i][idx]):
+                names.append(sources[i][0])
+                ra = np.append(ra, sources[i][1]*np.pi/180.)
+                dec = np.append(dec, sources[i][2]*np.pi/180.)
+                J_factors = np.append(J_factors, sources[i][idx])
+                break
+
+    J_factors = 10**J_factors
+
+    return names, ra, dec, J_factors
+
+J_indices_map = {"0.1": [6], "0.2": [9], "0.5": [12], "10": [15], "min": [6,9,12,15], "max": [15,12,9,6]}
+
 # Source encoding - 0:name; 1:RA; 2:Dec; 3,4,5:r_h; 6,7,8:J0.1; 9,10,11:J0.2; 12,13,14:J0.5; 15,16,17:J10
 sources = np.genfromtxt(args.sources, delimiter=",", dtype=("U20",float,float,float,float,float,float,float,float,float,float,float,float,float,float,float,float,float))
-names = []
-ra  = np.array([])
-dec = np.array([])
-J_factors = np.array([])
-for i in range(len(sources)):
-    for idx in [15,12,9,6]:
-        if not np.isnan(sources[i][idx]):
-            names.append(sources[i][0])
-            ra = np.append(ra, sources[i][1]*np.pi/180.)
-            dec = np.append(dec, sources[i][2]*np.pi/180.)
-            J_factors = np.append(J_factors, sources[i][idx])
-            break
-J_factors = 10**J_factors
+names, ra, dec, J_factors = extract_source_info(sources, args.J_type, J_indices_map)
 
 outfile = "sig_trials_" + args.channel + "_" + str(args.mass) + ".txt"
 
-def run_one_trial(ni, bkg_pdfs, sig_pdfs, source, channel, mass):
-    counts, bins = sig_pdfs[channel][mass]["Counts"], sig_pdfs[channel][mass]["Bins"]
-    data = generate_rand_from_pdf(counts,bins[:-1],ni)
+def run_one_trial(ni_source, data, bkg_pdfs, sig_pdfs, source, channel, mass, J_factors):
     
     # Recover signal
     N = len(data)
     llh = []
     ll = 0
     
-    for i in range(int(N*1.05)):
-        ll = 0
-        i = float(i)
-        for n in data:
-            bkg_bin = np.max(np.where((bkg_pdfs[source][channel][mass]["Bins"] <= n) == True)) # Find left edge of bin
-            bkg_prob = bkg_pdfs[source][channel][mass]["Counts"][bkg_bin] # Get probability of being in that bin
-            bkg_prob /= np.sum(bkg_pdfs[source][channel][mass]["Counts"])
-            sig_bin = np.max(np.where((sig_pdfs[channel][mass]["Bins"] <= n) == True))
-            sig_prob = sig_pdfs[channel][mass]["Counts"][sig_bin]
-            sig_prob /= np.sum(sig_pdfs[channel][mass]["Counts"])
-            comb = (i/N)*sig_prob + (1-(i/N))*bkg_prob
-            try:
-                ll += np.log(comb)
-            except:
-                ll = 0 # Datapoint is guaranteed to be background --> comb = 1 --> log(comb) = 0
-        
-        llh.append(-ll)
+    bkg_bins = np.searchsorted(bkg_pdfs[source][channel][mass]["Bins"], data, "right")-1 # Find left edges of bins
+    bkg_probs = np.array(bkg_pdfs[source][channel][mass]["Counts"][bkg_bins], dtype=float) # Get probabilities for bins
+    bkg_probs /= np.sum(bkg_pdfs[source][channel][mass]["Counts"]) # Normalize probabilities
+    sig_bins = np.searchsorted(sig_pdfs[channel][mass]["Bins"], data, "right")-1
+    sig_probs = np.array(sig_pdfs[channel][mass]["Counts"][sig_bins], dtype=float)
+    sig_probs /= np.sum(sig_pdfs[channel][mass]["Counts"])
     
-    bf = np.argmin(llh)
-    ts = 2*(llh[0] - llh[bf])
+    if (ni_source != 0):
+        for ns in range(int(ni_source*1.05)):
+            ns = float(ns)
+            combs = (ns/N)*sig_probs + (1-(ns/N))*bkg_probs
+            llh.append(-1*np.sum(np.log(combs)))
+        
+        bf = np.argmin(llh)
+        ts = 2*(llh[0] - llh[bf])
+    
+    else:
+        for ns in range(int(len(data)/100)):
+            ns = float(ns)
+            combs = (ns/N)*sig_probs + (1-(ns/N))*bkg_probs
+            llh.append(-1*np.sum(np.log(combs)))
+    
+        bf = np.argmin(llh)
+        bf = int(np.round(bf*J_factors[names.index(source)]/np.sum(J_factors)))
+        ts = 2*(llh[0] - llh[bf])
+        ts *= J_factors[names.index(source)]/np.sum(J_factors)
     
     return bf, ts
 
-# Load in background trial information
-bkg_ns = []
-bkg_ts = []
-for i, source in enumerate(names):
-    source_ns = []
-    source_ts = []
-    bkg_trials = np.genfromtxt(args.bkg_folder+outfile.replace("sig","bkg"), delimiter="\t")
-    for t in range(len(bkg_trials)):
-        source_ns.append(bkg_trials[t][2*i])
-        source_ts.append(bkg_trials[t][2*i+1])
-    bkg_ns.append(np.median(source_ns))
-    bkg_ts.append(np.median(source_ts))
-
 #in_events = np.concatenate((np.arange(1,10,1,dtype=int), np.linspace(10,50,20,dtype=int)))
-in_events = np.concatenate((np.arange(1,10,1,dtype=int), np.linspace(10,30,20,dtype=int,endpoint=False), np.arange(30,300+30/2,30,dtype=int)))
+#in_events = np.concatenate((np.arange(1,10,1,dtype=int), np.linspace(10,30,20,dtype=int,endpoint=False), np.arange(30,300+30/2,30,dtype=int)))
+in_events = np.concatenate((np.arange(10,100,10,dtype=int),np.arange(100,1000+30/2,30,dtype=int)))
 
 t1 = time.time()
+
+data_bkg = []
+for source in names:
+    counts, bins = bkg_pdfs[source][args.channel][args.mass]["Counts"], bkg_pdfs[source][args.channel][args.mass]["Bins"]
+    data_bkg.append(generate_rand_from_pdf(counts,bins[:-1],args.num_bkg_events))
 
 for ni in in_events:
     rec = []
@@ -117,19 +127,18 @@ for ni in in_events:
         bfs = []
         tss = []
         
-        for source,jf in zip(names,J_factors):
-            ni_source = int(round(ni*jf/np.sum(J_factors)))
-            
-            if (ni_source == 0): # Background trial
-                bf_source = bkg_ns[names.index(source)]
-                ts_source = bkg_ts[names.index(source)]
-            else: # Signal trial
-                bf_source, ts_source = run_one_trial(ni_source, bkg_pdfs, sig_pdfs, source, args.channel, args.mass)
+        for j, source, in enumerate(names):
+            ni_source = int(round(ni*J_factors[j]/np.sum(J_factors)))
+            counts, bins = sig_pdfs[args.channel][args.mass]["Counts"], sig_pdfs[args.channel][args.mass]["Bins"]
+            data_sig = generate_rand_from_pdf(counts,bins[:-1],ni_source)
+            data = np.concatenate((data_bkg[j], data_sig))
+
+            bf_source, ts_source = run_one_trial(ni_source, data, bkg_pdfs, sig_pdfs, source, args.channel, args.mass, J_factors)
             
             bfs.append(bf_source)
             tss.append(ts_source)
         
-        bf_total = int(round(np.sum(bfs)))
+        bf_total = int(np.round(np.sum(bfs)))
         ts_total = np.sum(np.array(tss))
         rec.append(bf_total)
         ts_rec.append(ts_total)
